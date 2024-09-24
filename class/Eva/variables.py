dd904ac621357707a476c1faa81aabae5e1e42e7
pwang004 <p.wang@rubika-edu.ca>
2024-09-23 21:04:03 -0400

    Eva_classnotes

---
+++ class/Eva/variables.py
@@ -0,0 +1,23 @@
+from zipfile import error
+
+my_name ='Eva'
+string_number ='22'
+string_number02=('30.3')
+the_number =24
+new_variable =5.29
+print (my_name, the_number, new_variable)
+print (type(my_name))
+print (type(the_number))
+print (type(new_variable))
+print (the_number + new_variable)
+print (type(the_number + new_variable))
+print (int(string_number)+ 45 )
+print (string_number02 + str(45))
+print (string number + 20) error
+print (str(string_number02) + 20) error
+
+
+
+
+
+
